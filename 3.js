84894b8b00ed38caa737b85cdf7037f44173051b
Bradley <104906904+bradley-03@users.noreply.github.com>
2024-12-06 13:10:15 +0000

    3 solution 2

--- 3.js
+++ 3.js
@@ -2,6 +2,7 @@ const fs = require("fs")
 const input = fs.readFileSync("./inputs/3.txt", "utf8").trim()
 
 const mulRegexPattern = /mul\(\d+,\d+\)/g
+const mulRegexWithConditions = new RegExp("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)", "g")
 
 function executeMul(str) {
   const extractedNums = str.substring(str.indexOf("(") + 1, str.indexOf(")"))
@@ -17,5 +18,25 @@ function calculateMulCommands() {
   return finalTotal
 }
 
+function calculateMulCommandsWithConditions() {
+  const commands = input.match(mulRegexWithConditions)
+
+  let enabled = true
+  let total = 0
+  for (let command of commands) {
+    if (command === "do()") {
+      enabled = true
+    } else if (command === "don't()") {
+      enabled = false
+    } else if (enabled === true) {
+      total += executeMul(command)
+    }
+  }
+
+  return total
+}
+
 // final output Solution 1
 console.log(`Solution 1: ${calculateMulCommands()}`)
+// final output Solution 2
+console.log(`Solution 2: ${calculateMulCommandsWithConditions()}`)
